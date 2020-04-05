8354981eb772e3fba18474990e6c84e61b368421
John Fields <jjfields24@outlook.com>
2020-04-05 04:22:12 -0400

    changed DataArea.js

--- src/components/DataArea.js
+++ src/components/DataArea.js
@@ -1,37 +1,38 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 import DataTable from "./DataTable";
 import Nav from "./Nav";
 import API from "../utils/API";
 import "../styles/DataArea.css";
+import DataAreaContext from "../utils/DataAreaContext";
 
-export default class DataArea extends Component {
-  state = {
-    users: [{}],
+const DataArea = () => {
+  const [developerState, setDeveloperState] = useState({
+    users: [],
     order: "descend",
-    filteredUsers: [{}]
-  }
+    filteredUsers: [],
+    headings: [
+      { name: "Image", width: "10%", order: "descend" },
+      { name: "name", width: "10%", order: "descend" },
+      { name: "phone", width: "20%", order: "descend" },
+      { name: "email", width: "20%", order: "descend" },
+      { name: "dob", width: "10%", order: "descend" }
+    ]
+  });
 
-  headings = [
-    { name: "Image", width: "10%" },
-    { name: "Name", width: "10%" },
-    { name: "Phone", width: "20%" },
-    { name: "Email", width: "20%" },
-    { name: "DOB", width: "10%" }
-  ]
+  const handleSort = heading => {
+    let currentOrder = developerState.headings
+      .filter(elem => elem.name === heading)
+      .map(elem => elem.order)
+      .toString();
 
-  handleSort = heading => {
-    if (this.state.order === "descend") {
-      this.setState({
-        order: "ascend"
-      })
+    if (currentOrder === "descend") {
+      currentOrder = "ascend";
     } else {
-      this.setState({
-        order: "descend"
-      })
+      currentOrder = "descend";
     }
 
     const compareFnc = (a, b) => {
-      if (this.state.order === "ascend") {
+      if (currentOrder === "ascend") {
         // account for missing values
         if (a[heading] === undefined) {
           return 1;
@@ -41,8 +42,10 @@ export default class DataArea extends Component {
         // numerically
         else if (heading === "name") {
           return a[heading].first.localeCompare(b[heading].first);
+        } else if (heading === "dob") {
+          return a[heading].age - b[heading].age;
         } else {
-          return a[heading] - b[heading];
+          return a[heading].localeCompare(b[heading]);
         }
       } else {
         // account for missing values
@@ -54,50 +57,61 @@ export default class DataArea extends Component {
         // numerically
         else if (heading === "name") {
           return b[heading].first.localeCompare(a[heading].first);
-        } else {
-          return b[heading] - a[heading];
+        }else if (heading === "dob") {
+          return b[heading].age - a[heading].age;
+        }  else {
+          return b[heading].localeCompare(a[heading]);
         }
       }
-
-    }
-    const sortedUsers = this.state.filteredUsers.sort(compareFnc);
-    this.setState({ filteredUsers: sortedUsers });
-  }
-
-  handleSearchChange = event => {
-    console.log(event.target.value);
-    const filter = event.target.value;
-    const filteredList = this.state.users.filter(item => {
-      // merge data together, then see if user input is anywhere inside
-      let values = Object.values(item)
-        .join("")
-        .toLowerCase();
-      return values.indexOf(filter.toLowerCase()) !== -1;
+    };
+    const sortedUsers = developerState.filteredUsers.sort(compareFnc);
+    const updatedHeadings = developerState.headings.map(elem => {
+      elem.order = elem.name === heading ? currentOrder : elem.order;
+      return elem;
     });
-    this.setState({ filteredUsers: filteredList });
-  }
 
-  componentDidMount() {
+    setDeveloperState({
+      ...developerState,
+      filteredUsers: sortedUsers,
+      headings: updatedHeadings
+    });
+  };
+
+  const handleSearchChange = event => {
+    const filter = event.target.value;
+    const filteredList = developerState.users.filter(item => {
+      let values = item.name.first.toLowerCase() + " " + item.name.last.toLowerCase();
+      console.log(filter, values)
+    if(values.indexOf(filter.toLowerCase()) !== -1){
+      return item
+    };
+    });
+
+    setDeveloperState({ ...developerState, filteredUsers: filteredList });
+  };
+
+  ///https://stackoverflow.com/questions/53120972/how-to-call-loading-function-with-react-useeffect-only-once
+  useEffect(() => {
     API.getUsers().then(results => {
-      this.setState({
+      console.log(results.data.results);
+      setDeveloperState({
+        ...developerState,
         users: results.data.results,
         filteredUsers: results.data.results
       });
     });
-  }
+  }, []);
 
-  render() {
-    return (
-      <>
-        <Nav handleSearchChange={this.handleSearchChange} />
-        <div className="data-area">
-          <DataTable
-            headings={this.headings}
-            users={this.state.filteredUsers}
-            handleSort={this.handleSort}
-          />
-        </div>
-      </>
-    );
-  }
-}
+  return (
+    <DataAreaContext.Provider
+      value={{ developerState, handleSearchChange, handleSort }}
+    >
+      <Nav />
+      <div className="data-area">
+        {developerState.filteredUsers.length > 0 ? <DataTable /> : <div></div>}
+      </div>
+    </DataAreaContext.Provider>
+  );
+};
+
+export default DataArea;
